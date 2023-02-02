f6a1f958a3c97d504c942df62d42c165fb0d982f
larrytu327 <larrytu327@gmail.com>
2023-02-02 10:39:59 +0800

    Commit 1 - Verbal questions

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // 1.1. What is the difference between a parameter and an argument?
+//An argument is used for functions and are the variables defined for that function and when it's called, while parameter is the variables that are defined when the function is declared. Function parameters are the variables that define the function, while function arguments are the real values passed to the function.
 
 // 1.2. Within a function, what is the difference between return and console.log?
+//console.log will print out whatever you'd like, while return is used for returning values from a function when it's called.
 
 // 1. 3. What are the implications of the ability of a function to return a value?
+//The function was invoked and returned a value with proper arguments that fulfilled the defined parameters of the function.
 
 // 2. calculateCube
 function calculateCube(num) {
