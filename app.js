ef53b8b3461151e2d036794906bcefa7f5953525
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-02-01 21:59:32 -0500

    Commit 2 - Cube Calculation

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
+    return num ** 3
 }
-
+console.log (calculateCube (3));
 // 3. isAVowel
 function isAVowel(letter) {
     // YOUR CODE HERE
