4eb26083f1b81feb57c32a89a7887d1d2397cc1f
nickycraig <nickycraig@gmail.com>
2023-02-02 10:12:26 -0800

    Commit 4 - Two Lengths

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ console.log(isAVowel("a"));
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
     // YOUR CODE HERE
+    return ([word1.length, word2.length]);
 }
+console.log(getTwoLengths("Hank", "Hippopopalous"));
 
 // 5. sumArray
 function sumArray(arr) {
