eef6daf7c389915dcbbcc11f86ba2b492ccab954
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-02-04 17:12:46 -0500

    Work in progress

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 1.1. What is the difference between a parameter and an argument?
-
+Within a function, a parameter is the placeholder location for the inputs that a function should receive while an argument is the actual value that is specified to fill the parameter locations.
 // 1.2. Within a function, what is the difference between return and console.log?
-
+Within a function, return is a statement that stops the execution of a function and enables other code to leverage the value that is passed on whereas console.log is a function that will display (log) the function's arguments.
 // 1. 3. What are the implications of the ability of a function to return a value?
 
 // 2. calculateCube
