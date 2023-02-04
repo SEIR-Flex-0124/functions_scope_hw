8327baaea353ae0a65b49dc0cb744d5dfcb8641c
larrytu327 <larrytu327@gmail.com>
2023-02-04 12:56:01 -0800

    Commit 5 - Sum Array

--- app.js
+++ app.js
@@ -37,8 +37,13 @@ console.log(getTwoLengths("Hank", "Hippopopalous"));
 
 // 5. sumArray
 function sumArray(arr) {
-    // YOUR CODE HERE
+    let sum = 0;
+    for (let i = 0;i < arr.length;i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
+console.log(sumArray([1,2,3,4,5,6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
