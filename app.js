77226123c7bd985249c33a7b50c33fb7b554a87c
mustafacev <mustafaorcuncevik@gmail.com>
2023-02-05 19:41:09 -0500

    Commit 5 - Sum Array

--- app.js
+++ app.js
@@ -42,7 +42,14 @@ function getTwoLengths(word1, word2) {
 // 5. sumArray
 function sumArray(arr) {
     // YOUR CODE HERE
-}
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) { // Creating a for...loop to iterate over the array
+          sum += arr[i];
+        }
+        return sum;
+      }
+      console.log(sumArray([1, 2, 3, 4, 5, 6]));  
+
 
 // 6.1 checkPrime
 function checkPrime(nums) {
