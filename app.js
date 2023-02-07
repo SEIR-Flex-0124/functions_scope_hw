2e68550059608d4e8d55f7a49d55153169e26fe3
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-02-07 17:28:59 -0500

    added code to isAVowel

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // 1.1. What is the difference between a parameter and an argument?
-  // Function parameters are the names listed in the functions definitions while arguments are the real values passed to the function. 
-  https://developer.mozilla.org/en-US/docs/Glossary/Parameter
-  // functions are the building blocks of prgorams. Functions are a resusable block of code written to perform a single purpose.
-  // Parameters become local variables inside the function body. 
+// Function parameters are the names listed in the functions definitions while arguments are the real values passed to the function.
+//developer.mozilla.org/en-US/docs/Glossary/Parameter
+// functions are the building blocks of prgorams. Functions are a resusable block of code written to perform a single purpose.
+// Parameters become local variables inside the function body.
 // 1.2. Within a function, what is the difference between return and console.log?
-  // Console.log will display the parameter in the console. Return statements stop executing the function and return the specified value (not within the console).
+// Console.log will display the parameter in the console. Return statements stop executing the function and return the specified value (not within the console).
 // 1. 3. What are the implications of the ability of a function to return a value?
-  // When a function returns a value, a value is retuned via a return statement to the caller of the function, after being implicity converted to the return type of the function in which it is defined. 
-  // source: https://www.ibm.com/docs/en/zos/2.1.0?topic=specifiers-function-return-values
+// When a function returns a value, a value is retuned via a return statement to the caller of the function, after being implicity converted to the return type of the function in which it is defined.
+// source: https://www.ibm.com/docs/en/zos/2.1.0?topic=specifiers-function-return-values
 
 // 2. calculateCube
-function calculateCube(num) {
+https: function calculateCube(num) {
   return num ** 3
 }
-console.log(calculateCube(5))
+console.log(calculateCube(5)) //expecting 125
 
 // 3. isAVowel
 function isAVowel(vowel) {
@@ -23,33 +23,41 @@ function isAVowel(vowel) {
     return false
   }
 }
-const vow = 'a' // variable 
-const nonVow = 'b' // variable 
-console.log(isAVowel('a'))
-console.log(isAVowel('b'))
+const vow = 'a' // variable
+const nonVow = 'b' // variable
+console.log(isAVowel('a')) //expecting true
+console.log(isAVowel('b')) //expecting false
+
+// OR the following is a way that eric did it in resources channel:
+// function isAVowel(letter) {
+//   if (letter.toLowerCase() === 'a' || letter.toLowerCase() === 'e' || letter.toLowerCase() === 'i' || letter.toLowerCase() === 'o' || letter.toLowerCase() === 'u') return true;
+//   return false;
+// }
+// console.log(isAVowel(a))
+// //however this did not run???
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-  return[word1.length, word2.length]
+  return [word1.length, word2.length]
 }
 console.log(getTwoLengths('Hank', 'Hippopopalous'))
 
 // 5. sumArray
 function sumArray(arr) {
   let sum = 0
-  for (let i=0; i<arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i]
   }
   return sum
 }
-
-console.log(sumArray([1, 2, 3, 4, 5, 6]));
+console.log(sumArray([1, 2, 3, 4, 5, 6])) //expecting 21
 
 // 6.1 checkPrime
 function checkPrime(num) {
   let prime = 0
   for (let i = 0; i < num.length; i++) {
-    if (num[i] % 1 === 0) {}
+    if (num[i] % 1 === 0) {
+    }
     prime += num[i]
   }
   return prime
