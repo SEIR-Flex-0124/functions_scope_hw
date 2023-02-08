e99e610aab038660c90482f7c6284cb1d5290247
undftdcreed <undftdcreed@gmail.com>
2023-02-08 10:24:26 -0800

    Check prime added

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ function calculateCube(num) {
 console.log(calculateCube(6));
 // 3. isAVowel
 function isAVowel(letter) {
-    return(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "A" || letter == "E" || letter == "O" || letter == "U")
+    if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "A" || letter == "E" || letter == "O" || letter == "U") {
+        return true;
+    } else {
+        return false;
+    }
 }
-console.log(isAVowel("E"));
+console.log(isAVowel("J"));
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
   let arr = []
@@ -37,13 +41,39 @@ console.log(sumArray([8,6,7,5,309]));
 // 6.1 checkPrime
 function checkPrime(num) {
     // YOUR CODE HERE
-}
+    if (num===1)
+    {
+        return false;
+    }
+    else if(num===2)
+    {
+        return true;
+    }else {
+        for(var x=2; x<num; x++){
+        if (num % x === 0)
+        {   return false;
+        }}
+        return true;
+    }
 
+}
+console.log(checkPrime(69))
+console.log(checkPrime(2))
+console.log(checkPrime(23))
 // 6.2 printPrimes
 function printPrimes(num) {
     // YOUR CODE HERE
+    let arrOfPrimes = [];
+     for(let i = 2; i <= num; i++) {
+       if(checkPrime(i)) {
+         arrOfPrimes.push(i);
+       }
+     }
+     return arrOfPrimes;
 }
-
+console.log(printPrimes(69))
+console.log(printPrimes(45))
+console.log((23))
 // 7. printLongestWord
 function printLongestWord(arr) {
     // YOUR CODE HERE
