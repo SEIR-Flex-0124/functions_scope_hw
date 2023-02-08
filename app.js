a46b921a8a7fd4e7816a99fa0ed4514cbee1965a
JacobPaul710 <jacobpaulscott@outlook.com>
2023-02-08 13:15:03 -0800

    Commit 5 - Sum Array

--- app.js
+++ app.js
@@ -69,9 +69,16 @@ function getTwoLengths(word1, word2) {
 console.log(getTwoLengths("Testing123", "Hellooooooooo, is this thing on?!"));
 
 // 5. sumArray
+
 function sumArray(arr) {
-    // YOUR CODE HERE
+    let sum = 0;
+     for (i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
 }
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
