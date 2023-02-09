0b5bfcf048f7770be27472787d97d6835a13a858
RyanWalck <rwalck33@gmail.com>
2023-02-08 20:21:51 -0500

    Completed

--- app.js
+++ app.js
@@ -1,43 +1,74 @@
 // 1.1. What is the difference between a parameter and an argument?
+//A Argument is the vaules that are declared within a function within the function when the function is called. A Parameter is the varibles that are defined when the function is declared. 
 
-// 1.2. Within a function, what is the difference between return and console.log?
+// 1.2. Within a function, what is the difference between return and console.log? 
+// Console. log is function that logs passed in argument to console. Return is keyword, which part of the return statement, used to end function execution.
 
 // 1. 3. What are the implications of the ability of a function to return a value?
+// A Return Vaule is when the vaules that a function returns when it completes. The ability of the function is how it works leading up to the return.
 
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
+    return num ** 3;
 }
+console.log(calculateCube(2))
 
 // 3. isAVowel
 function isAVowel(letter) {
-    // YOUR CODE HERE
+    return "aeiou".includes(letter.toLowerCase());
 }
+console.log(isAVowel("a"));
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
+    return [word1.length, word2.length];
 }
+console.log(getTwoLengths("Hank", "Hippopopalous"));
 
 // 5. sumArray
 function sumArray(arr) {
-    // YOUR CODE HERE
+    let sum = 0;
+    for (let i = 0; i< arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum;
 }
+ consosle.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
 function checkPrime(num) {
-    // YOUR CODE HERE
+    for (let i = 2; i < Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
+console.log(checkPrime(21));
 
 // 6.2 printPrimes
 function printPrimes(num) {
-    // YOUR CODE HERE
-}
+   for (let i = 2; i <= num; i++) {
+    if (checkPrime(i)) {
+        console.log(i);
+      }
+     }
+    }
+ console.log(printPriems(197));
+
 
 // 7. printLongestWord
 function printLongestWord(arr) {
-    // YOUR CODE HERE
+    let longestWord = "";
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i].length > longestWord.length) {
+            longestWord = arr[i]
+        }
+    }
+    return longestWord
 }
+
+console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "blob"]));
 
 // BONUS!
 
