c8731f8113702bbaf9d5c281823a1d11b32736b9
xoavc <vern407@gmail.com>
2023-02-25 14:42:59 -0500

    Functions Hw

--- app.js
+++ app.js
@@ -10,76 +10,82 @@
 
 // 1. 3. What are the implications of the ability of a function to return a value?
 
-isAVowel("a")
 
 // 2. calculateCube
 function calculateCube(num) {
-    // YOUR CODE HERE
-   let x = Math.cbrt(num)
+    return num **3;
+   console.log(calculateCube(5))
 
 }
 
 // 3. isAVowel
 function isAVowel(letter) {
-    // YOUR CODE HERE
-    if (letter === "a"){
-        console.log("Vowel")
-    }
-    else if(letter === "e"){
-        console.log("vowel")
-    }
-    else if(letter === "i"){
-        console.log("vowel")
-    }
-    else if(letter ==="o"){
-        console.log("vowel")
-    }
-    else(letter === "u"){
-        console.log("vowel")
-    }
-}
+//     if (letter === "a"){
+//         console.log("isAVowel")
+//     }
+//     else if(letter === "e"){
+//         console.log("isAVowel")
+//     }
+//     else if(letter === "i"){
+//         console.log("isAVowel")
+//     }
+//     else if(letter ==="o"){
+//         console.log("isAVowel")
+//     }
+//     else(letter === "u"){
+//         console.log("isAVowel")
+//     }
+// }
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
-    // YOUR CODE HERE
-
+    return (word1.length, word2.length);
+    
+    console.log(getTwoLengths("Hank", "Hippopopalous"));
 }
 
 // 5. sumArray
-function sumArray(arr) {
-    // YOUR CODE HERE
-    let sum = arr.reduce(function(a, b){
-        return a + b;
-    });
-    console.log(sum)
-   
+ function sumArray(arr) {
+    let sum = 0;
+       for (let i = 0; i < arr.length; i++){
+        sum += arr[i]
+       }
+       return sum;
 }
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 // 6.1 checkPrime
-function checkPrime(num) {
-    // YOUR CODE HERE
-    for (var i =2; i < num; i++){
-        if(n % i === 0) return false;
+ function checkPrime(num) {
+
+     for (let i =2; i < num; i++){
+         if(n % i === 0) {
+         return false;
     }
-    return n > 1; {
-        console.log(num)
-    }
+}  
+         return true;
+     
+        console.log(checkPrime(num))
+     
 
 
-}
+//}
 
 // 6.2 printPrimes
 function printPrimes(num) {
-    // YOUR CODE HERE
-    for (i=1; i<=100; i++){
-        console.log(num)
+    
+    for (i = 2; i <= num; i++){
+      if (checkPrime(i)){
+        console.log(i)
+      }
+        
     }
 
 }
+     console.log(PrintPrime(num))
 
-// 7. printLongestWord
+ //7. printLongestWord
 function printLongestWord(arr) {
-    // YOUR CODE HERE
+    
 }
 
 // BONUS!
